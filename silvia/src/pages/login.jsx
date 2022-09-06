013ad4bbc460d5a02eb83438a5d99e03ee463933
joshva115 <19486433@student.westernsydney.edu.au>
2022-09-06 14:03:00 +1000

    Login / Register Bootstrap
    Imported the styling for Login / Register in react-bootstrap, imported the general.css colours to override pre-existing colours on the boostrap components.
    
    Removed app.css - wasn't needed

--- silvia/src/pages/login.jsx
+++ silvia/src/pages/login.jsx
@@ -1,7 +1,64 @@
 import React from "react";
-
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import "./general.css";
 function login() {
-  return <div></div>;
+  return (
+    <Container className="d-flex align-items-center justify-content-center py-5">
+      <Card className="card shadow-lg p-5">
+        <Form>
+          <Card.Title className="pb-2 display-5 mt-5 text-center">
+            Login
+          </Card.Title>
+          <Form.Group className="mb-3 pt-3">
+            <Form.Label className="lead">Email address</Form.Label>
+            <Form.Control type="text" placeholder="Email Address" id="email" />
+            <Form.Text className="text-muted">
+              We'll <b>never</b> share your data with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label className="lead">Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              id="password"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Check type="checkbox" label="Remember me" />
+          </Form.Group>
+          <Form.Group className="col-md-12 text-center pt-1">
+            <Button
+              variant="btn btn-lg w-100"
+              className="custom-btn"
+              type="submit"
+            >
+              Login
+            </Button>
+          </Form.Group>
+          <Form.Group className="pb-2">
+            <Container className="text-left">
+              <Card className="text-center mt-3">
+                <Card.Footer className="border-0">
+                  <Card.Body className="text-center">
+                    {" "}
+                    Don't have an account?{" "}
+                    <a href="/register" className="text-dark">
+                      Create One
+                    </a>
+                  </Card.Body>
+                </Card.Footer>
+              </Card>
+            </Container>
+          </Form.Group>
+        </Form>
+      </Card>
+    </Container>
+  );
 }
 
 export default login;
--- silvia/src/pages/register.jsx
+++ silvia/src/pages/register.jsx
@@ -1,7 +1,73 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
 
 function register() {
-  return <div>register</div>;
+  return (
+    <Container>
+      <h3 class="text-left display-5 pt-4">Create Account</h3>
+      <Form className="row g-2 mt-3">
+        <Form.Group className="mb-1 col-md-6">
+          <Form.Label className="lead">First Name</Form.Label>
+          <Form.Control id="name" type="text" placeholder="Name" />
+        </Form.Group>
+        <Form.Group className="col-md-6 w-10">
+          <Form.Label className="lead">Surname</Form.Label>
+          <Form.Control id="surname" type="text" placeholder="Surname" />
+        </Form.Group>
+        <Form.Group className="col-md-6 w-10">
+          <Form.Label className="lead">Email Address</Form.Label>
+          <Form.Control type="text" placeholder="Email Address" id="email" />
+        </Form.Group>
+        <Form.Group className="mb-1 col-md-6">
+          <Form.Label className="lead">Username</Form.Label>
+          <Form.Control type="text" placeholder="Username" id="username" />
+        </Form.Group>
+        <Form.Group className="mb-1 col-md-4">
+          <Form.Label className="lead">Address</Form.Label>
+          <Form.Control type="text" placeholder="1234 Main St" id="address" />
+          <Form.Text className="text-muted">
+            We'll <b>never</b> share your information with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="col-md-4">
+          <Form.Label className="lead">City</Form.Label>
+          <Form.Control
+            type="text"
+            id="city"
+            placeholder="Sydney"
+            maxLength={10}
+          />
+        </Form.Group>
+        <Form.Group className="col-md-4">
+          <Form.Label className="lead">Zip</Form.Label>
+          <Form.Control type="text" id="zip" placeholder="2567" maxLength={4} />
+        </Form.Group>
+        <Form.Group className="mb-1 col-md-6">
+          <Form.Label className="lead">Password</Form.Label>
+          <Form.Control id="password" type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="col-md-6 w-10">
+          <Form.Label className="lead">Confirm Password</Form.Label>
+          <Form.Control
+            id="confirmPassword"
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Group>
+        <Form.Group class="col-md-12 text-center pt-3">
+          <Button
+            variant="btn btn-lg w-75"
+            className="custom-btn"
+            type="submit"
+          >
+            Register Details
+          </Button>
+        </Form.Group>
+      </Form>
+    </Container>
+  );
 }
 
 export default register;
