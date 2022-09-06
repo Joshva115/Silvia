cc250060ef29a1d1813e141360f569df9fdbb451
joshva115 <19486433@student.westernsydney.edu.au>
2022-09-07 09:18:12 +1000

    Testing Progress
    Importing schema is fine, formatting is fine however proper feedback to user needs to be investigated and fixed.
    
    When clicking on the email address / password both light up as invalid, should just be one instead of both.

--- silvia/src/pages/test.jsx
+++ silvia/src/pages/test.jsx
@@ -1,34 +1,23 @@
 import React from "react";
 import { Formik } from "formik";
+import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
 import Row from "react-bootstrap/Row";
+import InputGroup from "react-bootstrap/InputGroup";
+import Container from "react-bootstrap/Container";
 import * as yup from "yup";
-const schema = yup.object().shape({
-  firstName: yup.string().required(),
-  lastName: yup.string().required(),
-  username: yup.string().required(),
-  city: yup.string().required(),
-  state: yup.string().required(),
-  zip: yup.string().required(),
-  terms: yup.bool().required().oneOf([true], "Terms must be accepted"),
-});
+import { loginSchema } from "../schemas/validationSchema";
 
 const test = () => {
   return (
     <Formik
-      validationSchema={schema}
+      validationSchema={loginSchema}
       onSubmit={console.log}
       initialValues={{
-        firstName: "Mark",
-        lastName: "Otto",
-        username: "",
-        city: "",
-        state: "",
-        zip: "",
-        terms: false,
+        email: "",
+        password: "",
       }}
     >
       {({
@@ -41,108 +30,131 @@ const test = () => {
         errors,
       }) => (
         <Form noValidate onSubmit={handleSubmit}>
-          <Row className="mb-3">
-            <Form.Group as={Col} md="4" controlId="validationFormik01">
-              <Form.Label>First name</Form.Label>
-              <Form.Control
-                type="text"
-                name="firstName"
-                value={values.firstName}
-                onChange={handleChange}
-                isValid={touched.firstName && !errors.firstName}
-              />
-              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="4" controlId="validationFormik02">
-              <Form.Label>Last name</Form.Label>
-              <Form.Control
-                type="text"
-                name="lastName"
-                value={values.lastName}
-                onChange={handleChange}
-                isValid={touched.lastName && !errors.lastName}
-              />
+          <Container className="d-flex align-items-center justify-content-center py-5">
+            <Card className="card shadow-lg p-5">
+              <Form>
+                <Card.Title className="pb-2 display-5 mt-5 text-center">
+                  Login Test
+                </Card.Title>
+                <Form.Group className="mb-3 pt-3">
+                  <Form.Label className="lead">Email address</Form.Label>
+                  <Form.Control
+                    type="email"
+                    placeholder="Email Address"
+                    aria-describedby="inputGroupPrepend"
+                    name="email"
+                    value={values.email}
+                    onChange={handleChange}
+                    isInvalid={!!errors.email}
+                    onBlur={handleBlur}
+                  />
+                  <Form.Text className="text-muted">
+                    We'll <b>never</b> share your data with anyone else.
+                  </Form.Text>
+                </Form.Group>
 
-              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="4" controlId="validationFormikUsername">
-              <Form.Label>Username</Form.Label>
-              <InputGroup hasValidation>
-                <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-                <Form.Control
-                  type="text"
-                  placeholder="Username"
-                  aria-describedby="inputGroupPrepend"
-                  name="username"
-                  value={values.username}
-                  onChange={handleChange}
-                  isInvalid={!!errors.username}
-                />
-                <Form.Control.Feedback type="invalid">
-                  {errors.username}
-                </Form.Control.Feedback>
-              </InputGroup>
-            </Form.Group>
-          </Row>
-          <Row className="mb-3">
-            <Form.Group as={Col} md="6" controlId="validationFormik03">
-              <Form.Label>City</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="City"
-                name="city"
-                value={values.city}
-                onChange={handleChange}
-                isInvalid={!!errors.city}
-              />
+                <Form.Group className="mb-3">
+                  <Form.Label className="lead">Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    id="password"
+                    name="password"
+                    value={values.password}
+                    onChange={handleChange}
+                    isInvalid={!!errors.password}
+                    onBlur={handleBlur}
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Check type="checkbox" label="Remember me" />
+                </Form.Group>
+                <Form.Group className="col-md-12 text-center pt-1">
+                  <Button
+                    variant="btn btn-lg w-100"
+                    className="custom-btn"
+                    type="submit"
+                  >
+                    Login
+                  </Button>
+                </Form.Group>
+                <Form.Group className="pb-2">
+                  <Container className="text-left">
+                    <Card className="text-center mt-3">
+                      <Card.Footer className="border-0">
+                        <Card.Body className="text-center">
+                          {" "}
+                          Don't have an account?{" "}
+                          <a href="/register" className="text-dark">
+                            Create One
+                          </a>
+                        </Card.Body>
+                      </Card.Footer>
+                    </Card>
+                  </Container>
+                </Form.Group>
+              </Form>
+            </Card>
+          </Container>
 
-              <Form.Control.Feedback type="invalid">
-                {errors.city}
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="3" controlId="validationFormik04">
-              <Form.Label>State</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="State"
-                name="state"
-                value={values.state}
-                onChange={handleChange}
-                isInvalid={!!errors.state}
-              />
-              <Form.Control.Feedback type="invalid">
-                {errors.state}
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="3" controlId="validationFormik05">
-              <Form.Label>Zip</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Zip"
-                name="zip"
-                value={values.zip}
-                onChange={handleChange}
-                isInvalid={!!errors.zip}
-              />
+          {/* <Container>
+            <Row className="mb-3">
+              <Form.Group
+                as={Col}
+                md="12"
+                controlId="validationEmail"
+                className="pb-4"
+              >
+                <Form.Label>Email Address</Form.Label>
+                <InputGroup hasValidation className="pt-2">
+                  <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+                  <Form.Control
+                    type="email"
+                    placeholder="Email Address"
+                    aria-describedby="inputGroupPrepend"
+                    name="email"
+                    value={values.email}
+                    onChange={handleChange}
+                    isInvalid={!!errors.email}
+                  />
+                  <Form.Control.Feedback type="invalid" tooltip>
+                    {errors.email}
+                  </Form.Control.Feedback>
+                </InputGroup>
+              </Form.Group>
 
-              <Form.Control.Feedback type="invalid">
-                {errors.zip}
-              </Form.Control.Feedback>
-            </Form.Group>
-          </Row>
-          <Form.Group className="mb-3">
-            <Form.Check
-              required
-              name="terms"
-              label="Agree to terms and conditions"
-              onChange={handleChange}
-              isInvalid={!!errors.terms}
-              feedback={errors.terms}
-              feedbackType="invalid"
-              id="validationFormik0"
-            />
-          </Form.Group>
-          <Button type="submit">Submit form</Button>
+              <Form.Group
+                as={Col}
+                md="12"
+                controlId="validationFormikUsername2"
+              >
+                <Form.Label>Password</Form.Label>
+                <InputGroup hasValidation>
+                  <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    aria-describedby="inputGroupPrepend"
+                    name="password"
+                    value={values.password}
+                    onChange={handleChange}
+                    isInvalid={!!errors.password}
+                  />
+                  <Form.Control.Feedback type="invalid" tooltip>
+                    {errors.password}
+                  </Form.Control.Feedback>
+                </InputGroup>
+              </Form.Group>
+              <Form.Group className="col-md-12 text-center pt-4">
+                <Button
+                  variant="btn btn-lg w-50"
+                  className="custom-btn"
+                  type="submit"
+                >
+                  Login
+                </Button>
+              </Form.Group>
+            </Row>
+          </Container> */}
         </Form>
       )}
     </Formik>
