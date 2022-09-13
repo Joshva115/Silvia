2a21c42ce246cd05495c5a64fefac631a64e4517
joshva115 <19486433@student.westernsydney.edu.au>
2022-09-13 14:03:53 +1000

    Registration Progress
    Validated that blank boxes aren't acceptable

--- silvia/src/pages/test.jsx
+++ silvia/src/pages/test.jsx
@@ -4,7 +4,7 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
-import { loginSchema } from "../schemas/validationSchema";
+import { registerSchema } from "../schemas/validationSchema";
 import "./general.css";
 const onSubmit = async (values, actions) => {
   console.log(values);
@@ -16,11 +16,18 @@ const onSubmit = async (values, actions) => {
 const test = () => {
   return (
     <Formik
-      validationSchema={loginSchema}
+      validationSchema={registerSchema}
       onSubmit={onSubmit}
       initialValues={{
+        name: "",
+        surname: "",
         email: "",
+        username: "",
+        address: "",
+        city: "",
+        zip: "",
         password: "",
+        confirmPassword: "",
       }}
     >
       {({
@@ -40,11 +47,35 @@ const test = () => {
             <Form className="row g-2 mt-3">
               <Form.Group className="mb-1 col-md-6">
                 <Form.Label className="lead">First Name</Form.Label>
-                <Form.Control id="name" type="text" placeholder="Name" />
+                <Form.Control
+                  type="name"
+                  placeholder="First Name"
+                  id="name"
+                  value={values.name}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={errors.name && touched.name ? "input-error" : ""}
+                />
+                {errors.name && touched.name && (
+                  <p className="error">{errors.name}</p>
+                )}
               </Form.Group>
               <Form.Group className="col-md-6 w-10">
                 <Form.Label className="lead">Surname</Form.Label>
-                <Form.Control id="surname" type="text" placeholder="Surname" />
+                <Form.Control
+                  id="surname"
+                  type="text"
+                  placeholder="Surname"
+                  value={values.surname}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.surname && touched.surname ? "input-error" : ""
+                  }
+                />
+                {errors.surname && touched.surname && (
+                  <p className="error">{errors.surname}</p>
+                )}
               </Form.Group>
               <Form.Group className="col-md-6 w-10">
                 <Form.Label className="lead">Email Address</Form.Label>
@@ -52,7 +83,14 @@ const test = () => {
                   type="text"
                   placeholder="Email Address"
                   id="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={errors.email && touched.email ? "input-error" : ""}
                 />
+                {errors.email && touched.email && (
+                  <p className="error">{errors.email}</p>
+                )}
               </Form.Group>
               <Form.Group className="mb-1 col-md-6">
                 <Form.Label className="lead">Username</Form.Label>
@@ -60,7 +98,16 @@ const test = () => {
                   type="text"
                   placeholder="Username"
                   id="username"
+                  value={values.username}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.username && touched.username ? "input-error" : ""
+                  }
                 />
+                {errors.username && touched.username && (
+                  <p className="error">{errors.username}</p>
+                )}
               </Form.Group>
               <Form.Group className="mb-1 col-md-4">
                 <Form.Label className="lead">Address</Form.Label>
@@ -68,7 +115,16 @@ const test = () => {
                   type="text"
                   placeholder="1234 Main St"
                   id="address"
+                  value={values.address}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.address && touched.address ? "input-error" : ""
+                  }
                 />
+                {errors.address && touched.address && (
+                  <p className="error">{errors.address}</p>
+                )}
                 <Form.Text className="text-muted">
                   We'll <b>never</b> share your information with anyone else.
                 </Form.Text>
@@ -80,7 +136,14 @@ const test = () => {
                   id="city"
                   placeholder="Sydney"
                   maxLength={10}
+                  value={values.city}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={errors.city && touched.city ? "input-error" : ""}
                 />
+                {errors.city && touched.city && (
+                  <p className="error">{errors.city}</p>
+                )}
               </Form.Group>
               <Form.Group className="col-md-4">
                 <Form.Label className="lead">Zip</Form.Label>
@@ -89,7 +152,14 @@ const test = () => {
                   id="zip"
                   placeholder="2567"
                   maxLength={4}
+                  value={values.zip}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={errors.zip && touched.zip ? "input-error" : ""}
                 />
+                {errors.zip && touched.zip && (
+                  <p className="error">{errors.zip}</p>
+                )}
               </Form.Group>
               <Form.Group className="mb-1 col-md-6">
                 <Form.Label className="lead">Password</Form.Label>
@@ -97,7 +167,16 @@ const test = () => {
                   id="password"
                   type="password"
                   placeholder="Password"
+                  value={values.address}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.password && touched.password ? "input-error" : ""
+                  }
                 />
+                {errors.password && touched.password && (
+                  <p className="error">{errors.password}</p>
+                )}
               </Form.Group>
               <Form.Group className="col-md-6 w-10">
                 <Form.Label className="lead">Confirm Password</Form.Label>
@@ -105,7 +184,18 @@ const test = () => {
                   id="confirmPassword"
                   type="password"
                   placeholder="Password"
+                  value={values.confirmPassword}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.confirmPassword && touched.confirmPassword
+                      ? "input-error"
+                      : ""
+                  }
                 />
+                {errors.confirmPassword && touched.confirmPassword && (
+                  <p className="error">{errors.confirmPassword}</p>
+                )}
               </Form.Group>
               <Form.Group class="col-md-12 text-center pt-3">
                 <Button
