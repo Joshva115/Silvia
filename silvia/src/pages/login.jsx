3ebb66c510e651f625a38ce40665acd5f18af04c
joshva115 <19486433@student.westernsydney.edu.au>
2022-09-14 14:41:06 +1000

    Removed Page Validation
    Removed browser Email validation

--- silvia/src/pages/login.jsx
+++ silvia/src/pages/login.jsx
@@ -30,79 +30,75 @@ const login = () => {
         isValid,
         errors,
       }) => (
-        <Form onSubmit={handleSubmit}>
+        <Form noValidate onSubmit={handleSubmit}>
           <Container className="d-flex align-items-center justify-content-center py-5">
             <Card className="card shadow-lg p-5">
-              <Form>
-                <Card.Title className="pb-2 display-5 mt-5 text-center">
-                  Login Test
-                </Card.Title>
-                <Form.Group className="mb-3 pt-3">
-                  <Form.Label className="lead">Email address</Form.Label>
-                  <Form.Control
-                    type="email"
-                    placeholder="Email Address"
-                    aria-describedby="inputGroupPrepend"
-                    name="email"
-                    id="email"
-                    value={values.email}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    className={
-                      errors.email && touched.email ? "input-error" : ""
-                    }
-                  />
-                  {errors.email && touched.email && (
-                    <p className="error">{errors.email}</p>
-                  )}
-                  <Form.Text className="text-muted">
-                    We'll <b>never</b> share your data with anyone else.
-                  </Form.Text>
-                </Form.Group>
+              <Card.Title className="pb-2 display-5 mt-5 text-center">
+                Login Test
+              </Card.Title>
+              <Form.Group className="mb-3 pt-3">
+                <Form.Label className="lead">Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Email Address"
+                  aria-describedby="inputGroupPrepend"
+                  name="email"
+                  id="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={errors.email && touched.email ? "input-error" : ""}
+                />
+                {errors.email && touched.email && (
+                  <p className="error">{errors.email}</p>
+                )}
+                <Form.Text className="text-muted">
+                  We'll <b>never</b> share your data with anyone else.
+                </Form.Text>
+              </Form.Group>
 
-                <Form.Group className="mb-3">
-                  <Form.Label className="lead">Password</Form.Label>
-                  <Form.Control
-                    type="password"
-                    placeholder="Password"
-                    id="password"
-                    name="password"
-                    value={values.password}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    className={
-                      errors.password && touched.password ? "input-error" : ""
-                    }
-                  />
-                  {errors.password && touched.password && (
-                    <p className="error">{errors.password}</p>
-                  )}
-                </Form.Group>
-                <Form.Group className="col-md-12 text-center pt-1">
-                  <Button
-                    variant="btn btn-lg w-100"
-                    className="custom-btn"
-                    type="submit"
-                  >
-                    Login
-                  </Button>
-                </Form.Group>
-                <Form.Group className="pb-2">
-                  <Container className="text-left">
-                    <Card className="text-center mt-3">
-                      <Card.Footer className="border-0">
-                        <Card.Body className="text-center">
-                          {" "}
-                          Don't have an account?{" "}
-                          <a href="/register" className="text-dark">
-                            Create One
-                          </a>
-                        </Card.Body>
-                      </Card.Footer>
-                    </Card>
-                  </Container>
-                </Form.Group>
-              </Form>
+              <Form.Group className="mb-3">
+                <Form.Label className="lead">Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  id="password"
+                  name="password"
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.password && touched.password ? "input-error" : ""
+                  }
+                />
+                {errors.password && touched.password && (
+                  <p className="error">{errors.password}</p>
+                )}
+              </Form.Group>
+              <Form.Group className="col-md-12 text-center pt-1">
+                <Button
+                  variant="btn btn-lg w-100"
+                  className="custom-btn"
+                  type="submit"
+                >
+                  Login
+                </Button>
+              </Form.Group>
+              <Form.Group className="pb-2">
+                <Container className="text-left">
+                  <Card className="text-center mt-3">
+                    <Card.Footer className="border-0">
+                      <Card.Body className="text-center">
+                        {" "}
+                        Don't have an account?{" "}
+                        <a href="/register" className="text-dark">
+                          Create One
+                        </a>
+                      </Card.Body>
+                    </Card.Footer>
+                  </Card>
+                </Container>
+              </Form.Group>
             </Card>
           </Container>
         </Form>
